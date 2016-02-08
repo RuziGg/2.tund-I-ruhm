74018210f21c07c86f86166c4a763620d8ef9841
Ruslan Jegorov <rusljeg@greeny.cs.tlu.ee>
2016-02-08 11:25:19 +0200

    moosipurk

--- app.js
+++ app.js
@@ -14,25 +14,50 @@
 		
 		console.log('moosipurgi sees');
 		
+		// Koik muutujad mida muudetakse ja on rakendusega seotud defineeritakse siin
+		this.click_count = 0;
+		console.log(this);
+		
 		// Kui tahan Moosipurgile referenci siis kasitan THIS = MOOSIPURGI RAKENDUS ISE
 		this.init();
 		
 	};
 	
+	window.Moosipurk = Moosipurk; // Paneme muutja kulge
 	
 	// Koik funktsioonid lahevad Moosipurgi kulge
 	Moosipurk.prototype = {
 		
 		init: function(){
 			console.log('Rakendus laks toole');
+			
+			// Esimene Loogika oleks see, et kuulame hiireklikki nupul
+			this.bindMouseEvent();
+			
 		},
 		
 		bindMouseEvent: function(){
 			
+			document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+			
+		},
+		
+		addNewClick: function(event){
+			
+			console.log(event);
+			this.click_count++;
+			console.log(this.click_count);
+			
 		}
+		
 	};
 	
+	// kui leht laetud kaivitan Moosipurgi rakenduse
+	window.onload = function(){
+		
 	var app = new Moosipurk();
+	
+	}
 	
 	
 })();
